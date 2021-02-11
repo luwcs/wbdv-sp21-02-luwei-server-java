e8b40ff244222811866458e7394dae72615d6165
luwei <>
2021-02-10 18:46:18 -0800

    Created service.js

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -7,17 +7,38 @@ function AdminUserServiceClient() {
   this.url = 'https://wbdv-generic-server.herokuapp.com/api/luwei/users';
   // jannunzi
   var self = this;
-  function createUser(user) {
 
+  function createUser(user) {
+    return fetch(self.url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    }).then(function (response) {
+      return response.json()
+    })
   }
+
   function findAllUsers() {
     return fetch(self.url)
       .then(function (response) {
       return response.json()
     })
   }
+
   function findUserById(userId) { }
-  function updateUser(userId, user) { }
+
+  function updateUser(userId, user) {
+    return fetch(`${self.url}/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    }).then(response => response.json())
+  }
+  
   function deleteUser(userId) {
     return fetch(`${self.url}/${userId}`,
         {method: 'DELETE'})
--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -4,8 +4,9 @@ var $firstNameFld
 var $lastNameFld
 var $roleFld
 var $createBtn
+var $updateBtn
 var tableRows
-var userService = new AdminUserServiceClient()
+var userService
 
 
 var users = [
@@ -35,7 +36,7 @@ function renderUsers(users) {
             <span class="float-right">
               <i id="${i}" class="fa-2x fa fa-times wbdv-remove"></i>
               &nbsp;
-              <i class="fa-2x fa fa-pencil wbdv-edit"></i>
+              <i id="${user._id}" class="fa-2x fa fa-pencil wbdv-edit"></i>
             </span>
           </td>
         </tr>
@@ -44,25 +45,59 @@ function renderUsers(users) {
   }
 
   $(".wbdv-remove").click(deleteUser)
+
+  $(".wbdv-edit").click(editUser)
+}
+
+var selectedUser = null
+function editUser(event) {
+  var editBtn = $(event.target)
+  var theId = editBtn.attr("id")
+  selectedUser = users.find(user => user._id === theId)
+  $usernameFld.val(selectedUser.username)
+  $firstNameFld.val(selectedUser.firstname)
+  $lastNameFld.val(selectedUser.lastname)
+  $roleFld.val(selectedUser.role)
+}
+
+function updateUser() {
+  selectedUser.username = $usernameFld.val()
+  selectedUser.firstname = $firstNameFld.val()
+  selectedUser.lastname = $lastNameFld.val()
+  selectedUser.role = $roleFld.val()
+  userService.updateUser(selectedUser._id, selectedUser)
+    .then(function (status) {
+      var index = users.findIndex(user => user._id === selectedUser._id)
+      users[index] = selectedUser
+      renderUsers(users)
+    })
 }
 
 function deleteUser(event) {
-  var button = $(event.target)
-  var id = button.attr("id")
-  console.log(id)
-  users.splice(id, 1)
-  renderUsers(users)
+  var deleteBtn = $(event.target)
+  var theIndex = deleteBtn.attr("id")
+  console.log(theIndex)
+  var theId = users[theIndex]._id
+  userService.deleteUser(theId)
+  .then(function (status) {
+    users.splice(theIndex, 1)
+    renderUsers(users)
+  })
 }
 
 function createUser() {
   var newUser = {
     username: $usernameFld.val(),
+    password: $passwordFld.val(),
     firstname: $firstNameFld.val(),
     lastname: $lastNameFld.val(),
     role: $roleFld.val()
   }
-  users.push(newUser)
-  renderUsers(users)
+  userService.createUser(newUser)
+    .then(function (actualUser) {
+      users.push(actualUser)
+      renderUsers(users)
+    })
   $usernameFld.val("")
   $passwordFld.val("")
   $firstNameFld.val("")
@@ -77,15 +112,21 @@ function init() {
   $lastNameFld = $("#lastNameFld")
   $roleFld = $("#roleFld")
   $createBtn = $(".wbdv-create")
+  $updateBtn = $(".wbdv-update")
   tableRows = $(".wbdv-tbody")
+  userService = new AdminUserServiceClient()
 
   $createBtn.click(createUser)
+
+  $updateBtn.click(updateUser)
 
   userService.findAllUsers()
     .then(function (actualUsersFromServer) {
       users = actualUsersFromServer
       renderUsers(users)
     })
+
+
 }
 // wait until dom has loaded
 jQuery(init)
